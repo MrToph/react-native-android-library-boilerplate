779524ae94f4f12c209d8e46c22aaf7fd29f6184
Christoph Michel <christoph.michel.1992@gmail.com>
2016-11-26 18:31:54 +0100

    Initial

---
+++ android/src/main/java/io/cmichel/boilerplate/Module.java
@@ -0,0 +1,38 @@
+package io.cmichel.boilerplate;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.widget.Toast;
+
+public class Module extends ReactContextBaseJavaModule {
+
+  private static final String DURATION_SHORT_KEY = "SHORT";
+  private static final String DURATION_LONG_KEY = "LONG";
+
+  public LauncherModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  public String getName() {
+    return "Boilerplate";
+  }
+
+  @Override
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+    constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+    return constants;
+  }
+
+  @ReactMethod
+  public void show(String message, int duration) {
+    Toast.makeText(getReactApplicationContext(), message, duration).show();
+  }
+}
---
+++ index.android.js
@@ -0,0 +1,5 @@
+'use strict'
+
+import { NativeModules } from 'react-native'
+// name as defined via ReactContextBaseJavaModule's getName
+module.exports = NativeModules.Boilerplate
